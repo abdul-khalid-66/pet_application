1933c088467e3dcaf230cb564604e43b076c6a49
AK Project <abdulkhalidmasood66@gmail.com>
2025-08-09 03:02:39 +0500

    models added

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'seller_id',
+        'buyer_id'
+    ];
+
+    public function seller()
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
+
+    public function buyer()
+    {
+        return $this->belongsTo(User::class, 'buyer_id');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function latestMessage()
+    {
+        return $this->hasOne(Message::class)->latest();
+    }
 }
